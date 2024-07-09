6d45479c58035cd87f869268954c1489145d6979
Eduardo Del Balso <e.delbalso@gmail.com>
2024-07-08 18:14:22 -0700

    Installs next-ui

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,13 +1,16 @@
 import { GeistSans } from "geist/font/sans";
 import { type AppType } from "next/app";
+import {NextUIProvider} from "@nextui-org/react";
 
 import "~/styles/globals.css";
 
 const MyApp: AppType = ({ Component, pageProps }) => {
   return (
-    <div className={GeistSans.className}>
-      <Component {...pageProps} />
-    </div>
+    <NextUIProvider>
+      <div className={GeistSans.className}>
+        <Component {...pageProps} />
+      </div>
+    </NextUIProvider>
   );
 };
 
